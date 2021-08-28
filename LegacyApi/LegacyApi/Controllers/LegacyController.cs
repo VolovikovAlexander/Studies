631a7c4c163c634eea34d94840410db9a6f911c5
Volovikov Alexander <alexander.volovikov@grapecity.com>
2021-08-28 09:15:26 +0300

    Fixed the controller and changed some in the project documentation.

---
+++ LegacyApi/LegacyApi/Controllers/LegacyController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LegacyApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LegacyController : ControllerBase
+    {
+       
+        private readonly ILogger<LegacyController> _logger;
+
+        public LegacyController(ILogger<LegacyController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Проект подготовки к занятиям в ИГУ. Инструменты: \nC#, EF, MS SQL Server 2014\n\nДокументация: ../Docs\nЛогика проекта: ../LegacyCore\nНабор автотестов: ../LegacyTest\n\nhttps://github.com/VolovikovAlexander/Legacy";
+        }
+    }
+}
