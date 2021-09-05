2f202bb381037cf6e08b3b8d3e395735ee985cb1
Volovikov Alexander <trp2008@yandex.ru>
2021-09-05 06:47:08 +0300

    fix changes

--- LegacyApi/LegacyCore/Logic/BuildManagerThreads.cs
+++ LegacyApi/LegacyCore/Logic/BuildManagerThreads.cs
@@ -23,19 +23,19 @@ namespace LegacyCore.Logic
             _logic = _logic ?? new LogicBuildTransactions();
 
             // 1 Удалить старые записи
-            var removeRecord = Task.Run (() => _logic.RemoveRecords(period));
+            var removeRecord = Task.Factory.StartNew(() => _logic.RemoveRecords(period));
 
             // 2. Добавим новых клиентов
-            var addCustomers = Task.Run (() => _logic.AddCustomers(period));
+            var addCustomers = Task.Factory.StartNew(() => _logic.AddCustomers(period));
 
             // 3 Добавим счета
-            var addAccounts = Task.Run(() => _logic.AddAccounts(period));
+            var addAccounts = Task.Factory.StartNew(() => _logic.AddAccounts(period));
 
             // 4 Добавим контракты
-            var addContracts = Task.Run(() => _logic.AddContracts(period));
+            var addContracts = Task.Factory.StartNew(() => _logic.AddContracts(period));
 
             // 5 Добавим транзации
-            var addTransactions = Task.Run(() => _logic.AddTransactions(period));
+            var addTransactions = Task.Factory.StartNew(() => _logic.AddTransactions(period));
 
             var firtsGroup = new List<Task>()
             { removeRecord, addCustomers, addAccounts, addContracts };
@@ -45,8 +45,8 @@ namespace LegacyCore.Logic
 
 
             // Запускаем задачи
-            Task.WhenAll(firtsGroup.ToArray()).GetAwaiter().GetResult();
-            Task.WhenAll(secondGroup.ToArray()).GetAwaiter().GetResult();
+            Task.WaitAll(firtsGroup.ToArray());
+            Task.WaitAll(secondGroup.ToArray());
 
             return true;
         }
