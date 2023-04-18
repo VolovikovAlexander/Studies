bea5da4fad421f30aab93efacf0b2fef445f097b
Volovikov Alexander <trp2008@yandex.ru>
2023-04-18 11:59:54 +0300

    fix changes

---
+++ Yandex.Cloud/Src/Models/Act.py
@@ -0,0 +1,84 @@
+from Src.Models.Statuses import progress_status
+from Src.Models.Executor import executor
+from Src.Models.Contractor import contractor
+
+
+#
+# Модель документа /Акт проверки/
+#
+class act():
+    __amount = 0
+    __executor = None
+    __contractors = {}
+    __progress = None
+
+    def __init__(self):
+        self.__progress = progress_status.start
+
+
+    @property
+    def amount(self):
+        # Свойство: Сумма штрафа    
+        return self.__amount
+    
+    @amount.setter
+    def amount(self, value):
+        # Свойство: Сумма штрафа    
+        if value is None:
+            raise Exception("ОШИБКА! Параметр amount - не указан!")
+        
+        self.__amount = value
+
+
+    @property
+    def executor(self):
+        # Свойство: Исполнитель    
+        return self.__executor
+
+    def executor(self, value):
+        # Свойство: Исполнитель    
+        if value is None:
+            raise Exception("ОШИБКА! Параметр executor - не указан!")
+        
+        if not isinstance(value, executor):
+            raise Exception("ОШИБКА! Параметр executor - должен быть типом executor!")
+        
+
+    def contractors(self):
+        # Свойство: Список застройщиков   
+        items = list(self.__contractors)
+        return items
+        
+
+    def create(self_executor):
+        # Фабричный метод    
+        if not isinstance(self_executor, executor):
+            raise Exception("ОШИБКА! Параметр self_executor - должен быть типом executor!")
+           
+        result = act()
+        result.executor = self_executor
+        result.add(self_executor.contrator)
+
+        return result
+        
+
+    def add(self, self_contractor):
+        # Добавить в документ исполнителей
+        if self_contractor is None:
+            return
+        
+        if not isinstance(self_contractor, contractor):
+            raise Exception("ОШИБКА! Параметр self_contractor - должен быть типом contractor!")
+        
+        self.__contractors[self_contractor.guid] = self_contractor
+        self.add(self_contractor.parent)
+
+        
+        
+
+
+
+
+
+        
+        
--- Yandex.Cloud/Tests/test_models.py
+++ Yandex.Cloud/Tests/test_models.py
@@ -3,6 +3,7 @@ import unittest
 from Src.Models.Building  import building
 from Src.Models.Contractor import contractor
 from Src.Models.Executor import executor
+from Src.Models.Act import act
 
 #
 # Набор модульных тестов для проверки работы моделей данных
@@ -56,6 +57,23 @@ class models_tests(unittest.TestCase):
         # Проверки
         assert result is not None
         print(result.guid)
+
+
+    # Проверить создание документа /Акт/
+    def test_create_act(self):
+        # Подготовка
+        contractor_parent = contractor.create(name="test1", parent=None)
+        contractor_act = contractor.create(name="test2", parent=contractor_parent)
+        executor_act = executor.create(name="test3", self_contractor=contractor_act)
+
+        # Действие
+        result = act.create(self_executor=executor_act)
+        #result.add(contractor_act)
+
+        # Проверки
+        assert result is not None
+        assert len(result.contractors()) == 2
+
 
 
 
